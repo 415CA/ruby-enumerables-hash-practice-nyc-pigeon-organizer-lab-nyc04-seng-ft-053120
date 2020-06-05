638b421f99d88c2783109482b88048f7f4ffb25d
Chris Allen <chrisallen415@gmail.com>
2020-06-05 01:33:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,8 @@ def nyc_pigeon_organizer(pigeon_list)
   pigeon_data.each do |element_name, element|
     element.each do |element_value, pigeon_id|
       pigeon_id.each do |name|
-        new_pigeon_data[name] || = {}
-        new_pigeon_data[name][element_name] || = []
+        new_pigeon_data[name] ||= {}
+        new_pigeon_data[name][element_name] ||= []
         new_pigeon_data[name][element_name].push(element_value).to_s
       end
     end
