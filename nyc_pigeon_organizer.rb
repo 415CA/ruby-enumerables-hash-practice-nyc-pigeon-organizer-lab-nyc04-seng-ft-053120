0c3aec5087bc423037143d6cdd531c77935890be
Chris Allen <chrisallen415@gmail.com>
2020-06-05 00:50:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 pigeon_data = {
   :color => {
     :purple => ["Theo", "Peter Jr.", "Lucky"],
@@ -18,7 +20,24 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(pigeon_data)
-  # write your code here!
-  new_hash = {}
-  data
+  new_pigeon_data = {}
+  pigeon_data.each do |attribute_name, attribute|
+    attribute.each do |attribute_value, names|
+      names.each do
+        pigeon_data[name] = {}
+        pigeon_data[name][attribute_name] || []
+        pigeon_data[name][attribute_name].push(attribute_value).to_s
+      end
+    end
+  end
+
+  iterate through pigeon_data.keys to find array of "name"
+  loop through array
+  store
+ if !new_pigeon_data.include?("name")
+   new_pigeon_data.store("name", pigeon_data[keys].to_s)
+   store.
+ if name string does not exist
+   create hash with name as key and value as empty hash
+   add
 end
