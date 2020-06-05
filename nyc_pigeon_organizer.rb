c64ca15431dc278f0d610da20e43663cc20e5f29
Chris Allen <chrisallen415@gmail.com>
2020-06-05 00:53:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,24 +20,12 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(pigeon_data)
-  new_pigeon_data = {}
-  pigeon_data.each do |attribute_name, attribute|
-    attribute.each do |attribute_value, names|
+  pigeon_data.each do |element_name, element|
+    element.each do |element_value, names|
       names.each do
         pigeon_data[name] = {}
-        pigeon_data[name][attribute_name] || []
-        pigeon_data[name][attribute_name].push(attribute_value).to_s
+        pigeon_data[name][element_name] || []
+        pigeon_data[name][element_name].push(element_value).to_s
       end
     end
   end
-
-  iterate through pigeon_data.keys to find array of "name"
-  loop through array
-  store
- if !new_pigeon_data.include?("name")
-   new_pigeon_data.store("name", pigeon_data[keys].to_s)
-   store.
- if name string does not exist
-   create hash with name as key and value as empty hash
-   add
-end
